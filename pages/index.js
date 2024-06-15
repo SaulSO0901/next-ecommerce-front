3b27d508d8b2ccc13eab2259d7ebdb7e22c71eb0
Saul Salazar Ozuna <saulsalazar0909@gmai.com>
2024-06-14 18:32:36 -0700

    ecommerce front page started

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import Header from "@/components/Header";
+import Featured from "@/components/Featured";
+import {Product} from "@/models/Product";
+import {mongooseConnect} from "@/lib/mongoose";
+import NewProducts from "@/components/NewProducts";
+
+export default function HomePage({featuredProduct,featuredProduct2,newProducts}) {
+  return (
+    <div>
+      <Header />
+      <Featured product={featuredProduct} />
+      <NewProducts products={newProducts} />
+      <NewProducts products={featuredProduct2} />
+     
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const featuredProductId = '665fb000c52420c56aa533e2';
+  const featuredProductId2 = '665faff2c52420c56aa533db';
+  await mongooseConnect();
+  const featuredProduct = await Product.findById(featuredProductId);
+  const featuredProduct2 = await Product.find({},{ 'category': featuredProductId2,limit:10});
+  const newProducts = await Product.find({}, null, {sort: {'_id':-1}, limit:10});
+  return {
+    props: {
+      featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+      featuredProduct2: JSON.parse(JSON.stringify(featuredProduct2)),
+      newProducts: JSON.parse(JSON.stringify(newProducts)),
+    },
+  };
+}
